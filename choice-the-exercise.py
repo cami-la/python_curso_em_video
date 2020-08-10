53050a9d3ce6a1e65b8c73ed06f97f08d171e9a6
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2020-08-10 07:42:39 -0300

    Codes to run the choice the exercise and run the program.

---
+++ choice-the-exercise.py
@@ -0,0 +1,87 @@
+from .exercise001 import play as play001
+from .exercise002 import play as play002
+from .exercise003 import play as play003
+from .exercise004 import play as play004
+from .exercise005 import play as play005
+from .exercise006 import play as play006
+from .exercise007 import play as play007
+from .exercise008 import play as play008
+from .exercise009 import play as play009
+from .exercise010 import play as play010
+from .exercise011 import play as play011
+from .exercise012 import play as play012
+from .exercise013 import play as play013
+from .exercise014 import play as play014
+from .exercise015 import play as play015
+from .exercise016 import play as play016
+from .exercise017 import play as play017
+from .exercise018 import play as play018
+from .exercise019 import play as play019
+from .exercise020 import play as play020
+
+
+def exercise():
+  choice_exercise = int(input("Choice the number of exercise: "))
+  print("\n")
+
+  if (choice_exercise == 1):
+    print("Python Exercise 001:")
+    play001()
+  elif (choice_exercise == 2):
+    print("Python Exercise 002:")
+    play002()  
+  elif (choice_exercise == 3):
+    print("Python Exercise 003:")
+    play003()
+  elif (choice_exercise == 4):
+    print("Python Exercise 004:")
+    play004()
+  elif (choice_exercise == 5):
+    print("Python Exercise 005:")
+    play005()  
+  elif (choice_exercise == 6):
+    print("Python Exercise 006:")
+    play006()
+  elif (choice_exercise == 7):
+    print("Python Exercise 007:")
+    play007()  
+  elif (choice_exercise == 8):
+    print("Python Exercise 008:")
+    play008()  
+  elif (choice_exercise == 9):
+    print("Python Exercise 009:")
+    play009()
+  elif (choice_exercise == 10):
+    print("Python Exercise 010:")
+    play010()
+  elif (choice_exercise == 11):
+    print("Python Exercise 011:")
+    play011()  
+  elif (choice_exercise == 12):
+    print("Python Exercise 012:")
+    play012()
+  elif (choice_exercise == 13):
+    print("Python Exercise 013:")
+    play013()
+  elif (choice_exercise == 14):
+    print("Python Exercise 014:")
+    play014()
+  elif (choice_exercise == 15):
+    print("Python Exercise 015:")
+    play015()  
+  elif (choice_exercise == 16):
+    print("Python Exercise 016:")
+    play016() 
+  elif (choice_exercise == 17):
+    print("Python Exercise 017:")
+    play017()   
+  elif (choice_exercise == 18):
+    print("Python Exercise 018:")
+    play018()  
+  elif (choice_exercise == 19):
+    print("Python Exercise 019:")
+    play019() 
+  elif (choice_exercise == 20):
+    print("Python Exercise 020:")
+    play020()   
+
---
+++ main.py
@@ -0,0 +1,3 @@
+from list_of_exercises.choice_the_exercise import exercise
+
+exercise()
